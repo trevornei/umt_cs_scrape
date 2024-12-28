4488e9c9c329f2b488dfbdaf752a90fe29dbca76
trevornei <117675657+trevornei@users.noreply.github.com>
2024-12-28 14:32:36 -0700

    Added a for loop that iterates over the table and creates a list of core_headers/sub_table titles.

--- cs_catalog/main.py
+++ cs_catalog/main.py
@@ -10,7 +10,11 @@ response = requests.get(url)
 soup = bs4.BeautifulSoup(response.content, "html.parser")
 
 table = soup.find('table', class_='sc_courselist')
-print(table.prettify())
+# print(table.prettify())
+
+# Iterates through each core_header/sub_table title.
+for core_header in table.find_all('tr', class_='areaheader'):
+    print(core_header.prettify())
 # Create a class for sub_table.
 class sub_table:
     
