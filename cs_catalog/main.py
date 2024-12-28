901ab3d1f710fe50083d4545275a5e825aa9bc82
trevornei <117675657+trevornei@users.noreply.github.com>
2024-12-28 12:41:48 -0700

    Created a nested class for each row that will be apart of a sub_table.

--- cs_catalog/main.py
+++ cs_catalog/main.py
@@ -1,9 +1,30 @@
 import bs4
 import requests
-import random
+import json
 
 url = "https://catalog.umt.edu/colleges-schools-programs/humanities-sciences/computer-science/bs-computer-science-software-engineering/" 
 
+
 response = requests.get(url)
 
-soup = bs4.BeautifulSoup(response.text, "html.parser")
+soup = bs4.BeautifulSoup(response.content, "html.parser")
+
+# Create a class for sub_table.
+class sub_table:
+    
+    def __init__(self, core_header, rows, school, course, credit_hour):
+        self.core_header = core_header
+        self.rows = []
+        self.school = school
+        self.course = course
+        self.credit_hour = credit_hour
+        # Create a nested class for each row.
+        class row:
+            def __init__(self, school, course, credit_hour):
+                self.school = school
+                self.course = course
+                self.credit_hour = credit_hour
+
+# Create a class instance for each sub_tabel object.
+# Ref ~/cs_catalog/Data_Schema.md
+
